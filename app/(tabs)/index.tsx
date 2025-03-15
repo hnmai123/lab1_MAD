a9e64ee40e7b4b5fa4300f696056306afb695f20
Huy Nhat Mai <nhathuylk0123@gmail.com>
2025-03-16 00:14:03 +1100

    add web request

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import { View, StyleSheet, Button, TextInput } from 'react-native';
+import {useState} from 'react';
+import { View, StyleSheet, Button, TextInput, Text } from 'react-native';
 
 
 export default function HomeScreen() {
-  const [text, onChangeText] = React.useState('Enter URL');
+  const [text, onChangeText] = React.useState('https://reactnative.dev/movies.json');
+
+  const [webData, setwebData] = useState("");  // State and React hooks
+  const request = new XMLHttpRequest();
+
+  function _handlePressButtonAsync() {
+    request.onreadystatechange = e => {
+      if (request.readyState !== 4) {
+        return;
+      }
+      if (request.status === 200) {
+        console.log("Success" + request.responseText);
+        setwebData("Status " + request.status + " " + request.statusText + " " + request.responseText + " " + request.response);
+      } else {
+        console.warn('error');
+        setwebData("Error: " + request.status + " " + request.statusText + " " + request.responseText);
+      }
+    };
+    request.open('GET', text);
+    request.send();
+  }
 
   return (
     <View style = {styles.containerColumn}>
@@ -14,10 +35,11 @@ export default function HomeScreen() {
           value = {text}
         />
         <Button
-          title = "Click me"
-          onPress = {() => alert("Stop pressing my button!" + text)}
+          title = "Go Request"
+          onPress = {() => _handlePressButtonAsync()}
           />
       </View>
+      <Text>{webData}</Text>
     </View>
   );
 }
